27165d6c50a6ce3d8da880934c81666358bb7130
ycccck <yc.zeng@outlook.com>
2024-12-11 23:58:54 -0800

    sketch.js

--- sketch.js
+++ sketch.js
@@ -2,18 +2,20 @@
 // reference tutorial How to create collisions, walls, and barriers in the P5.js programming language; https://www.youtube.com/watch?v=JV5XBmaQdIA
 // extend the collison system using chatGPT
 // images and assets from https://itch.io/game-assets Sprout Land
+// sound from Pixbay, backgournd sound: 0 Bubblegum | Cute BGM, 1 Popcorn | Cute BGM, 4 Magic Keys | Cute BGM by bluelike_u, animal sound: Mjau3 by freesound_community, E3Ko by freesound_community, Short Chicken Noise by Alex_Jauk
 
 let player;
 let land;
 let house;
-let char1;
-let char2;
-let char3;
-let item1;
-let item2;
-let item3;
+let char1, char2, char3;
+let item1, item2, item3;
+let bgSound;
+let bgSound1, bgSound2, bgSound3;
+let iSound1, iSound2, iSound3;
 let obstacles = [];
 let items = [];
+let playButton;
+let gameStarted = false; // boolean to track if game has started
 
 function preload() {
   land = loadImage("land.png");
@@ -24,12 +26,33 @@ function preload() {
   item1 = loadImage("heart.png");
   item2 = loadImage("milk.png");
   item3 = loadImage("egg.png");
+
+  bgSound = loadSound("bgCat.mp3");
+  bgSound1 = loadSound("bgCat.mp3");
+  bgSound2 = loadSound("bgCow.mp3");
+  bgSound3 = loadSound("bgHen.mp3");
+
+  iSound1 = loadSound("cat.mp3");
+  iSound2 = loadSound("cow.mp3");
+  iSound3 = loadSound("hen.mp3");
 }
 
 function setup() {
   let canvas = createCanvas(800, 800);
   canvas.parent("farmyard");
-  player = new Player(width / 2, 200);
+
+  playButton = createButton("Start Game");
+  playButton.position(CENTER);
+  playButton.style("position", "relative");
+  playButton.style("background-color", "#F0EDC9");
+  playButton.style("color", "#38680a");
+  playButton.style("font-size", "20px");
+  playButton.style("font-weight", "bold");
+  playButton.style("border-radius", "5px");
+  playButton.style("border-width", "3px");
+  playButton.style("border-color", "#A4C287");
+  playButton.parent("farmyard");
+  playButton.mousePressed(startGame);
 
   // obstacles
   obstacles.push(new Obstacle(355, 160, 30, 320));
@@ -41,7 +64,6 @@ function setup() {
   obstacles.push(new Obstacle(110, 445, 30, 20));
   obstacles.push(new Obstacle(220, 445, 30, 20));
   obstacles.push(new Obstacle(465, 240, 30, 20));
-  obstacles.push(new Obstacle(210, 470, 5, 15));
   obstacles.push(new Obstacle(260, 600, 30, 320));
   obstacles.push(new Obstacle(310, 700, 100, 50));
   obstacles.push(new Obstacle(125, 560, 30, 20));
@@ -51,14 +73,13 @@ function setup() {
   obstacles.push(new Obstacle(665, 400, 200, 30));
   obstacles.push(new Obstacle(680, 450, 100, 100));
   obstacles.push(new Obstacle(635, 500, 45, 40));
-  obstacles.push(new Obstacle(635, 530, 10, 20));
   obstacles.push(new Obstacle(485, 130, 80, 80));
   obstacles.push(new Obstacle(650, 100, 200, 50));
   obstacles.push(new Obstacle(610, 140, 15, 30));
   obstacles.push(new Obstacle(665, 175, 10, 10));
   obstacles.push(new Obstacle(700, 680, 60, 60));
   obstacles.push(new Obstacle(420, 670, 20, 15));
-  obstacles.push(new Obstacle(480, 710, 5, 20));
+  obstacles.push(new Obstacle(480, 710, 10, 20));
   // house
   obstacles.push(new Obstacle(120, 600, 60, 60));
   obstacles.push(new Obstacle(650, 265, 60, 50));
@@ -74,10 +95,15 @@ function draw() {
   land.resize(width, height);
   image(land, width / 2, height / 2);
 
+  // if game hasn't started, show play button
+  if (!gameStarted) {
+    return;
+  }
+
   // house door locations
   ellipse(120, 640, 20, 20);
   ellipse(650, 300, 20, 20);
-  ellipse(550, 620, 20, 20);
+  ellipse(540, 620, 20, 20);
 
   house.resize(100, 100);
   image(house, 120, 620);
@@ -92,30 +118,38 @@ function draw() {
   char1.resize(35, 35);
   char2.resize(50, 35);
   char3.resize(35, 35);
-  
+
   // resize objects for pickup
-  item1.resize(25, 25);
+  item1.resize(20, 15);
   item2.resize(25, 25);
   item3.resize(25, 25);
-
-  // call player
-  player.update();
-  player.draw();
 
   // show items
   for (let item of items) {
     item.draw();
   }
 
+  // call player
+  player.update();
+  player.draw();
+
   // check for collisions with items
   colliItem();
 }
 
-  // create 2 of each item and place them outside obstacles
+// start the game when play button is clicked
+function startGame() {
+  gameStarted = true;
+  bgSound.loop();
+  playButton.hide();
+  player = new Player(width / 2, 200);
+}
+
+// create 5 of each item and place them outside obstacles
 function createItems() {
   let itemPositions = [];
-  
-  while (itemPositions.length < 6) {
+
+  while (itemPositions.length < 15) {
     let newItem = createVector(random(150, 650), random(150, 600));
     let validPosition = true;
 
@@ -125,7 +159,7 @@ function createItems() {
         newItem.x > obstacle.x - obstacle.w / 2 && // item is to the right of the obstacle's left side
         newItem.x < obstacle.x + obstacle.w / 2 && // item is to the left of the obstacle's right side
         newItem.y > obstacle.y - obstacle.h / 2 && // item is below the obstacle's top side
-        newItem.y < obstacle.y + obstacle.h / 2    // item is above the obstacle's bottom side
+        newItem.y < obstacle.y + obstacle.h / 2 // item is above the obstacle's bottom side
       ) {
         validPosition = false;
         break;
@@ -133,7 +167,12 @@ function createItems() {
     }
 
     // check if the item is placed out of bounds (not within 100px from the edge)
-    if (newItem.x < 100 || newItem.x > width - 100 || newItem.y < 100 || newItem.y > height - 100) {
+    if (
+      newItem.x < 100 ||
+      newItem.x > width - 100 ||
+      newItem.y < 100 ||
+      newItem.y > height - 100
+    ) {
       validPosition = false;
     }
 
@@ -142,17 +181,17 @@ function createItems() {
     }
   }
 
-  // create items at the valid positions
-  items.push(new Item(itemPositions[0].x, itemPositions[0].y, item1)); 
-  items.push(new Item(itemPositions[1].x, itemPositions[1].y, item1));
-  items.push(new Item(itemPositions[2].x, itemPositions[2].y, item2)); 
-  items.push(new Item(itemPositions[3].x, itemPositions[3].y, item2));  
-  items.push(new Item(itemPositions[4].x, itemPositions[4].y, item3));  
-  items.push(new Item(itemPositions[5].x, itemPositions[5].y, item3)); 
+  // create 5 items of each type at valid positions
+  for (let i = 0; i < 5; i++) {
+    items.push(new Item(itemPositions[i].x, itemPositions[i].y, item1));
+    items.push(new Item(itemPositions[i + 5].x, itemPositions[i + 5].y, item2));
+    items.push(
+      new Item(itemPositions[i + 10].x, itemPositions[i + 10].y, item3)
+    );
+  }
 }
 
-
-  // for each item, check if player collides with it and handle according to character type
+// for each item, check if player collides with it and handle according to character type
 function colliItem() {
   for (let i = items.length - 1; i >= 0; i--) {
     let item = items[i];
@@ -160,13 +199,14 @@ function colliItem() {
 
     if (distToItem < player.radius + 10) {
       if (player.player === char1 && item.image === item1) {
-
-        items.splice(i, 1);  // Remove the obj1 item
+        items.splice(i, 1); // remove the obj1 item
+        iSound1.play();
       } else if (player.player === char2 && item.image === item2) {
-
-        items.splice(i, 1);  
+        items.splice(i, 1);
+        iSound2.play();
       } else if (player.player === char3 && item.image === item3) {
-        items.splice(i, 1);  
+        items.splice(i, 1);
+        iSound3.play();
       }
     }
   }
@@ -203,7 +243,7 @@ class Player {
     let newX = this.x + mvmt.x;
     let newY = this.y + mvmt.y;
 
-    // check collison
+    // check collision
     if (!this.collides(newX, newY)) {
       this.x = newX;
       this.y = newY;
@@ -241,20 +281,35 @@ class Player {
     return false;
   }
 
-  // check collision with doors to change character
+  // check collision with doors to change character and background sound
   colliDoor(newX, newY) {
     // door 1 (ellipse at (120, 640))
     if (dist(newX, newY, 120, 640) < 20) {
-      this.player = char1; 
+      this.player = char1;
+      this.cgSound(bgSound1);
     }
     // door 2 (ellipse at (650, 300))
     if (dist(newX, newY, 650, 300) < 20) {
-      this.player = char2; 
+      this.player = char2;
+      this.cgSound(bgSound2);
     }
     // door 3 (ellipse at (550, 620))
     if (dist(newX, newY, 550, 620) < 20) {
-      this.player = char3; 
+      this.player = char3;
+      this.cgSound(bgSound3);
     }
+  }
+
+  // change background sound based on the character
+  cgSound(newSound) {
+    // stop the current sound if it's playing
+    if (bgSound.isPlaying()) {
+      bgSound.stop();
+    }
+
+    // play the new background sound
+    bgSound = newSound;
+    bgSound.loop();
   }
 
   // draw the current character
